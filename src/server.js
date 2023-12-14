793e33fbe7f6f6387b38f5df4ee87a858f0882d3
padungaz <padung1999@gmail.com>
2023-12-14 12:50:24 +0700

    #32

--- src/server.js
+++ src/server.js
@@ -2,7 +2,7 @@ require('dotenv').config()
 const express = require('express');
 const configViewEngine = require('./config/viewEngine');
 const webRoutes = require('./routes/web');
-const mysql = require('mysql2');
+const connection = require('./config/db');
 
 const app = express()
 const port = process.env.PORT || 8888
@@ -12,15 +12,6 @@ const hostname = process.env.HOST_NAME
 configViewEngine(app)
 
 // test connection
-// create the connection to database
-const connection = mysql.createConnection({
-    host: 'localhost',
-    port: 3307,
-    user: 'root',
-    password: '123456',
-    database: 'hoidanit'
-});
-
 connection.query(
     'select * from Users u',
     function (err, results, fields) {
